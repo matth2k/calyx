b06c665e375fc2ddd0d06c4c44b91689cf3dea37
matth2k <mrh259@cornell.edu>
2023-01-24 21:55:37 -0500

    read and write still has conflicts

--- primitives/arbiter.sv
+++ primitives/arbiter.sv
@@ -18,8 +18,8 @@ module arbiter_2 #(
    input wire logic read_en1,
    // Dynamic port memory
    input wire logic mem_write_done,
-   input wire logic mem_read_done
-   input wire [WIDTH-1:0] mem_out,
+   input wire logic mem_read_done,
+   input wire logic [WIDTH-1:0] mem_out,
 
    output logic [WIDTH-1:0] out0,
    output logic [WIDTH-1:0] out1,
@@ -54,15 +54,15 @@ module arbiter_2 #(
       read_fsm <= 2'd0;
     end else if (read_fsm == 2'd0) begin
       if (read_en0) begin
-        read_fsm <= 2'd1;
-      end else if (read_en1) begin
         read_fsm <= 2'd2;
+      end else if (read_en1) begin
+        read_fsm <= 2'd3;
       end
     end else if (read_fsm == 2'd1) begin
       if (read_en1) begin
-        read_fsm <= 2'd2;
+        read_fsm <= 2'd3;
       end else if (read_en0) begin
-        read_fsm <= 2'd1;
+        read_fsm <= 2'd2;
       end
     end else if (read_fsm == 2'd2) begin
       if (mem_read_done) begin
@@ -82,27 +82,37 @@ module arbiter_2 #(
   // 2 - busy port 0
   // 3 - busy port 1
 
-  wire logic arbiter_write_busy = read_fsm > 2'd1;
+  wire logic arbiter_write_busy = write_fsm > 2'd1;
   wire logic arbiter_write_idle = !arbiter_write_busy;
   wire logic write_fsm_state0 = write_fsm == 2'd0;
   wire logic write_fsm_state1 = write_fsm == 2'd1;
   wire logic write_fsm_state2 = write_fsm == 2'd2;
   wire logic write_fsm_state3 = write_fsm == 2'd3;
+
+  logic lastOpRead;
+  always_ff @(posedge clk) begin
+    if (arbiter_read_busy)
+      lastOpRead <= 1'b1;
+    else if (arbiter_write_busy)
+      lastOpRead <= 1'b0;
+  end
+
+  wire logic stallWrite = arbiter_read_busy;
   
   always_ff @(posedge clk) begin
     if (reset) begin
       write_fsm <= 2'd0;
-    end else if (write_fsm == 2'd0) begin
+    end else if (write_fsm == 2'd0 && !stallWrite) begin
       if (write_en0) begin
-        write_fsm <= 2'd1;
+        write_fsm <= 2'd2;
       end else if (write_en1) begin
-        write_fsm <= 2'd2;
+        write_fsm <= 2'd3;
       end
-    end else if (write_fsm == 2'd1) begin
+    end else if (write_fsm == 2'd1 && !stallWrite) begin
       if (write_en1) begin
-        write_fsm <= 2'd2;
+        write_fsm <= 2'd3;
       end else if (write_en0) begin
-        write_fsm <= 2'd1;
+        write_fsm <= 2'd2;
       end
     end else if (write_fsm == 2'd2) begin
       if (mem_write_done) begin
@@ -133,24 +143,27 @@ module arbiter_2 #(
             read_done1 <= 1'b1;
             out1 <= mem_out;
           end
+          mem_read_en <= 1'b0;
         end
       end else begin
-        read_done0 <= 1'b0;
-        read_done1 <= 1'b0;
         if (read_fsm_state0) begin
           if (read_en0) begin
             mem_read_en <= 1'b1;
+            read_done0 <= 1'b0;
             mem_addr <= addr0;
           end else if (read_en1) begin
             mem_read_en <= 1'b1;
+            read_done1 <= 1'b0;
             mem_addr <= addr1;
           end
         end else if (read_fsm_state1) begin
           if (read_en1) begin
             mem_read_en <= 1'b1;
+            read_done1 <= 1'b0;
             mem_addr <= addr1;
           end else if (read_en0) begin
             mem_read_en <= 1'b1;
+            read_done0 <= 1'b0;
             mem_addr <= addr0;
           end
         end
@@ -173,33 +186,46 @@ module arbiter_2 #(
           end else if (write_fsm_state3) begin
             write_done1 <= 1'b1;
           end
+          mem_write_en <= 1'b0;
         end
-      end else begin
-        write_done0 <= 1'b0;
-        write_done1 <= 1'b0;
+      end else if (!stallWrite) begin
         if (write_fsm_state0) begin
           if (write_en0) begin
             mem_write_en <= 1'b1;
+            write_done0 <= 1'b0;
             mem_addr <= addr0;
             mem_in <= in0;
           end else if (write_en1) begin
-            mem_read_en <= 1'b1;
+            mem_write_en <= 1'b1;
+            write_done1 <= 1'b0;
             mem_addr <= addr1;
             mem_in <= in1;
           end
         end else if (write_fsm_state1) begin
           if (write_en1) begin
             mem_write_en <= 1'b1;
+            write_done1 <= 1'b0;
             mem_addr <= addr1;
             mem_in <= in1;
           end else if (write_en0) begin
-            mem_read_en <= 1'b1;
+            mem_write_en <= 1'b1;
+            write_done0 <= 1'b0;
             mem_addr <= addr0;
             mem_in <= in0;
           end
         end
       end
     end
+  end
+
+  int fd;
+  initial begin
+    fd = $fopen("/home/matth2k/calyx/arb_out.log", "w");
+  end
+  always_ff @(posedge clk) begin
+    $fdisplay(fd, "port0_go: %b, port1_go: %b", write_en0 | read_en0, write_en1 | read_en1);
+    $fdisplay(fd, "port0_done: %b, port1_done: %b", write_done0 | read_done0, write_done1 | read_done1);
+    $fdisplay(fd, "read_fsm: %d, write_fsm: %d", read_fsm, write_fsm);
   end
 
 endmodule
